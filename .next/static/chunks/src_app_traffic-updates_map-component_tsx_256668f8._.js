c3e2f04e838bcce6a3c561c03a0373796dafb2d9
AVOR <56428739+EnguerrandB@users.noreply.github.com>
2025-07-02 11:22:53 +0200

    Add .next/static/chunks/src_app_traffic-updates_map-component_tsx_256668f8._.js 1751448172784

---
+++ .next/static/chunks/src_app_traffic-updates_map-component_tsx_256668f8._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/app/traffic-updates/map-component.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/_a1a9ed4c._.js",
+  "static/chunks/src_app_traffic-updates_map-component_tsx_947bf001._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/app/traffic-updates/map-component.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
