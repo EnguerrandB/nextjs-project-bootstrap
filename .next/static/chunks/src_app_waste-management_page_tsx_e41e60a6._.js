dbb76a489b0e01cdd3ef86f860e871562f05103e
AVOR <56428739+EnguerrandB@users.noreply.github.com>
2025-07-02 11:22:33 +0200

    Add .next/static/chunks/src_app_waste-management_page_tsx_e41e60a6._.js 1751448153518

---
+++ .next/static/chunks/src_app_waste-management_page_tsx_e41e60a6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d9435d3c._.js",
+  "static/chunks/src_e38b94f0._.js"
+],
+    source: "dynamic"
+});
