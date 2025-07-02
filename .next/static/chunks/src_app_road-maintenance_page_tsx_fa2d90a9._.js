426d4a714bcf5a8799ecc9671020564ded180f1f
AVOR <56428739+EnguerrandB@users.noreply.github.com>
2025-07-02 11:22:20 +0200

    Add .next/static/chunks/src_app_road-maintenance_page_tsx_fa2d90a9._.js 1751448139845

---
+++ .next/static/chunks/src_app_road-maintenance_page_tsx_fa2d90a9._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_b40e9f19._.js"
+],
+    source: "dynamic"
+});
